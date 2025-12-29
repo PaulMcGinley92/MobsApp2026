43b49ff943661c1487f0410a7646f1b38917a59c
Paul McGinley <pauldmcginley@gmail.com>
2025-12-29 20:43:03 +0000

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,20 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'recipe-details',
+    loadComponent: () => import('./pages/recipe-details/recipe-details.page').then( m => m.RecipeDetailsPage)
+  },
+  {
+    path: 'favourites',
+    loadComponent: () => import('./pages/favourites/favourites.page').then( m => m.FavouritesPage)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./pages/settings/settings.page').then( m => m.SettingsPage)
+  },
 ];
