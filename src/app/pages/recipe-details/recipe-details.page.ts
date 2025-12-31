3edfdce85d35445102a178c12a38bde76982c197
Paul McGinley <pauldmcginley@gmail.com>
2025-12-31 00:16:43 +0000

    Displays recipe instructions and ingredients on details page

--- src/app/pages/recipe-details/recipe-details.page.ts
+++ src/app/pages/recipe-details/recipe-details.page.ts
@@ -4,17 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
 import { SpoonacularService } from 'src/app/services/spoonacular';
+import { IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.page.html',
   styleUrls: ['./recipe-details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonList, IonItem, IonLabel]
 })
 export class RecipeDetailsPage {
 
   recipe: any;
+  ingredients: any[] = [];
+  steps: any[] = [];
 
   constructor(private route: ActivatedRoute, private api: SpoonacularService) {}
 
@@ -30,9 +33,11 @@ export class RecipeDetailsPage {
 }*/
 
     this.api.getRecipeInformation(id).subscribe({
-      next: (data) => {
+      next: (data: any) => {
         console.log('Details API returned:', data);
         this.recipe = data;
+        this.ingredients = data.extendedIngredients ?? [];
+        this.steps = data.analyzedInstructions?.[0]?.steps ?? [];
       },
       error: (err) => console.log('Details API ERROR:', err),
     });
