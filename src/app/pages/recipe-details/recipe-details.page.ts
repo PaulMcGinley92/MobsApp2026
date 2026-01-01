598d04e263c98f03a19daa339dbeabe3a2d98e35
Paul McGinley <pauldmcginley@gmail.com>
2026-01-01 17:09:46 +0000

    Added the Favourites button on the recipe page

--- src/app/pages/recipe-details/recipe-details.page.ts
+++ src/app/pages/recipe-details/recipe-details.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
 import { SpoonacularService } from 'src/app/services/spoonacular';
 import { IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
@@ -11,7 +11,7 @@ import { IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
   templateUrl: './recipe-details.page.html',
   styleUrls: ['./recipe-details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonList, IonItem, IonLabel]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonList, IonItem, IonLabel, IonButton]
 })
 export class RecipeDetailsPage {
 
@@ -19,6 +19,7 @@ export class RecipeDetailsPage {
   ingredients: any[] = [];
   steps: any[] = [];
   units: 'metric' | 'us' = 'metric';
+  isFavourite = false;
 
   constructor(private route: ActivatedRoute, private api: SpoonacularService) {}
   
@@ -47,8 +48,38 @@ if (!idStr || Number.isNaN(id) || id <= 0) {
         this.recipe = data;
         this.ingredients = data.extendedIngredients ?? [];
         this.steps = data.analyzedInstructions?.[0]?.steps ?? [];
+        this.loadFavouriteStatus();
       },
       error: (err: any) => console.log('Details API ERROR:', err),
     });
   }
+
+  
+
+  loadFavouriteStatus() {
+  const raw = localStorage.getItem('favourites');
+  const favs = raw ? JSON.parse(raw) : [];
+  this.isFavourite = favs.some((f: any) => f.id === this.recipe?.id);
+}
+
+toggleFavourite() {
+  const raw = localStorage.getItem('favourites');
+  let favs = raw ? JSON.parse(raw) : [];
+
+  if (this.isFavourite) {
+    favs = favs.filter((f: any) => f.id !== this.recipe.id);
+  } else {
+    favs.push({
+      id: this.recipe.id,
+      title: this.recipe.title,
+      image: this.recipe.image,
+    });
+    this.isFavourite = true;
+  }
+
+  localStorage.setItem('favourites', JSON.stringify(favs));
+  this.isFavourite = !this.isFavourite;
+
+  console.log('Updated favourites:', favs);
+}
 }
